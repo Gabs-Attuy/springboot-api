bd7d3ff51fe74c378c12e1eda8875dffb9da85e3
Gabs-Attuy <gabrielattuy1010@gmail.com>
2024-07-19 18:26:45 -0300

    Beto ANIMAL

---
+++ api-pratica/src/main/java/com/example/api_pratica/ApiPraticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_pratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPraticaApplication.class, args);
+	}
+
+}
